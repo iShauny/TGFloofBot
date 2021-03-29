1208c516a676578d13f03ee0099578872f1bc9d7
Shaun <35963613+iShauny@users.noreply.github.com>
2021-03-29 08:23:59 +0100

    Delete services directory

--- services/decorators.py
+++
@@ -1,8 +0,0 @@
-from telegram.ext import Updater, InlineQueryHandler, CommandHandler
-
-
-def command(cmd):
-    def wrapper():
-        super.dispatcher.add_handler(CommandHandler(cmd.__name__, cmd))
-
-    return wrapper
