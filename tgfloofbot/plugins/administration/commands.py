ae257d964beec49bd8d63160fe5a0d33020eed33
Shaun <35963613+iShauny@users.noreply.github.com>
2021-04-14 10:52:31 +0100

    Auto formatting

--- tgfloofbot/plugins/administration/commands.py
+++ tgfloofbot/plugins/administration/commands.py
@@ -17,6 +17,7 @@ from ...logger import LOG
 from . import models
 from . import exceptions
 
+
 @loader.custom
 def administration_custom(client: TGFloofbotClient):
 
@@ -26,42 +27,38 @@ def administration_custom(client: TGFloofbotClient):
         try:
             admin_group = client.updater.bot.get_chat(chat_id=admin_group_id)
         except telegram.error.TelegramError:
-            LOG.critical("Unable to resolve the group ID set in the config! Defaulting to context group ID.")
+            LOG.critical(
+                "Unable to resolve the group ID set in the config! Defaulting to context group ID."
+            )
             admin_group = update.effective_chat
 
         return admin_group
 
     class WarnCommandArgs(pydantic.BaseModel):
-        bad_user: str = pydantic.Field(
-            None, description="A raw user ID")
-        warn_message: str = pydantic.Field(
-            None, description="The warning reason")
+        bad_user: str = pydantic.Field(None, description="A raw user ID")
+        warn_message: str = pydantic.Field(None, description="The warning reason")
 
     class UsernoteCommandArgs(pydantic.BaseModel):
-        bad_user: Optional[str] = pydantic.Field(
-            None, description="A raw user ID")
-        warn_message: Optional[str] = pydantic.Field(
-            None, description="The note")
+        bad_user: Optional[str] = pydantic.Field(None, description="A raw user ID")
+        warn_message: Optional[str] = pydantic.Field(None, description="The note")
 
     @loader.command(name="warn", help="warn a user", admin=True)
     def warn_command(
         client: TGFloofbotClient,
         update: Update,
         context: CallbackContext,
-        args: WarnCommandArgs
+        args: WarnCommandArgs,
     ) -> None:
         warn_helper(client, update, context, args, False)
-        return
 
     @loader.command(name="note", help="adds a moderation note for a user", admin=True)
     def usernote_command(
         client: TGFloofbotClient,
         update: Update,
         context: CallbackContext,
-        args: UsernoteCommandArgs
+        args: UsernoteCommandArgs,
     ) -> None:
         warn_helper(client, update, context, args, True)
-        return
 
     def warn_helper(client, update, context, args, is_note):
         user = update.effective_user
@@ -69,9 +66,11 @@ def administration_custom(client: TGFloofbotClient):
         admin_chat = _resolve_admin_group_id(Update)
 
         if admin_chat.type != "group":  # group only command
-            context.bot.send_message(chat_id=chat, text="This command can only be used for groups.")
+            context.bot.send_message(
+                chat_id=chat, text="This command can only be used for groups."
+            )
             return
-        
+
         try:
             bad_user = client.updater.bot.get_chat_member(chat_id=admin_chat.id, user_id=args.bad_user).user  # type: ignore
             reason = args.warn_message
@@ -85,7 +84,7 @@ def administration_custom(client: TGFloofbotClient):
                 date_added=datetime.datetime.now(timezone.utc),
                 warned_by=user.username,
                 warned_by_id=user.id,
-                reason=reason
+                reason=reason,
             )
 
             if is_note:
@@ -95,16 +94,30 @@ def administration_custom(client: TGFloofbotClient):
             client.db.commit()
 
         except Exception as e:
-            LOG.error(f"Error occurred when attempting to insert a {'warning' if is_note is False else 'usernote'} into the database: " + str(e))
-            context.bot.send_message(chat_id=chat.id, text=f"A fatal error occurred trying to insert the {'warning' if is_note is False else 'usernote'} into the database.")
+            LOG.error(
+                f"Error occurred when attempting to insert a {'warning' if is_note is False else 'usernote'} into the database: "
+                + str(e)
+            )
+            context.bot.send_message(
+                chat_id=chat.id,
+                text=f"A fatal error occurred trying to insert the {'warning' if is_note is False else 'usernote'} into the database.",
+            )
             return
         if not is_note:
-            try: 
-                context.bot.send_message(chat_id=bad_user.id, text=f"You have been warned in *{admin_chat.title}*\. Reason: *{reason}*", parse_mode="MarkdownV2")
+            try:
+                context.bot.send_message(
+                    chat_id=bad_user.id,
+                    text=f"You have been warned in *{admin_chat.title}*\. Reason: *{reason}*",
+                    parse_mode="MarkdownV2",
+                )
             except Exception:
-                context.bot.send_message(chat_id=chat.id, text="Unable to DM the user to notify them of their warning.")
+                context.bot.send_message(
+                    chat_id=chat.id,
+                    text="Unable to DM the user to notify them of their warning.",
+                )
 
-        context.bot.send_message(chat_id=chat.id, text=f"*⚠️ User {bad_user.name} {'warned' if is_note is False else 'noted'} by {user.username} with reason {reason}\.*", parse_mode="MarkdownV2")
-
-
-
+        context.bot.send_message(
+            chat_id=chat.id,
+            text=f"*⚠️ User {bad_user.name} {'warned' if is_note is False else 'noted'} by {user.username} with reason {reason}\.*",
+            parse_mode="MarkdownV2",
+        )
