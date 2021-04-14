af46b66eff0be7fe33ced703e002da74ed2e276c
Shaun <35963613+iShauny@users.noreply.github.com>
2021-04-14 11:41:22 +0100

    Changed to timezone aware

--- tgfloofbot/plugins/administration/models.py
+++ tgfloofbot/plugins/administration/models.py
@@ -3,12 +3,14 @@ import datetime
 
 from ...models import ORMBase
 
+def tz_aware_now() -> datetime.datetime:
+    return datetime.datetime.now(datetime.timezone.utc)
 
 class Warning(ORMBase):
     __tablename__ = "tg_warnings"
     id = Column(Integer, primary_key=True, nullable=False)
     user_id = Column(Integer, nullable=False)
-    date_added = Column(DateTime, default=datetime.datetime.utcnow, nullable=False)
+    date_added = Column(DateTime, default=tz_aware_now, nullable=False)
     forgiven = Column(Boolean, default=False, nullable=False)
     forgiven_by = Column(String(), nullable=True)
     forgiven_by_id = Column(Integer, nullable=True)
